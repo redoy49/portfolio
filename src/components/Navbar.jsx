25cf13133d7844c5b834c96978a39728b1541620
Redoy Al Hasan <mdredoyhasan49@gmail.com>
2026-03-04 14:51:02 +0600

    feat(navbar): improve mobile dropdown layout and responsiveness

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -12,19 +12,29 @@ const Navbar = () => {
   const navLinks = (
     <>
       <li>
-        <HashLink smooth to="/#home">Home</HashLink>
+        <HashLink smooth to="/#home">
+          Home
+        </HashLink>
       </li>
       <li>
-        <HashLink smooth to="/#about">About Me</HashLink>
+        <HashLink smooth to="/#about">
+          About Me
+        </HashLink>
       </li>
       <li>
-        <HashLink smooth to="/#projects">Projects</HashLink>
+        <HashLink smooth to="/#projects">
+          Projects
+        </HashLink>
       </li>
       <li>
-        <HashLink smooth to="/#education">Education</HashLink>
+        <HashLink smooth to="/#education">
+          Education
+        </HashLink>
       </li>
       <li>
-        <HashLink smooth to="/#contact">Contact Me</HashLink>
+        <HashLink smooth to="/#contact">
+          Contact Me
+        </HashLink>
       </li>
     </>
   );
@@ -50,15 +60,27 @@ const Navbar = () => {
           <div className="flex items-center gap-1">
             {/* Theme toggle */}
             <label className="swap swap-rotate">
-              <input type="checkbox" className="theme-controller" value="synthwave"/>
+              <input
+                type="checkbox"
+                className="theme-controller"
+                value="synthwave"
+              />
               {/* Sun Icon */}
-              <svg className="swap-off h-7 w-7 xl:h-9 xl:w-9 fill-current text-slate-600" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
-                <path d="M5.64,17l-.71.71a1,1,0,0,0,0,1.41,1,1,0,0,0,1.41,0l.71-.71A1,1,0,0,0,5.64,17ZM5,12a1,1,0,0,0-1-1H3a1,1,0,0,0,0,2H4A1,1,0,0,0,5,12Zm7-7a1,1,0,0,0,1-1V3a1,1,0,0,0-2,0V4A1,1,0,0,0,12,5ZM5.64,7.05a1,1,0,0,0,.7.29,1,1,0,0,0,.71-.29,1,1,0,0,0,0-1.41l-.71-.71A1,1,0,0,0,4.93,6.34Zm12,.29a1,1,0,0,0,.7-.29l.71-.71a1,1,0,1,0-1.41-1.41L17,5.64a1,1,0,0,0,0,1.41A1,1,0,0,0,17.66,7.34ZM21,11H20a1,1,0,0,0,0,2h1a1,1,0,0,0,0-2Zm-9,8a1,1,0,0,0-1,1v1a1,1,0,0,0,2,0V20A1,1,0,0,0,12,19ZM18.36,17A1,1,0,0,0,17,18.36l.71.71a1,1,0,0,0,1.41,0,1,1,0,0,0,0-1.41ZM12,6.5A5.5,5.5,0,1,0,17.5,12,5.51,5.51,0,0,0,12,6.5Zm0,9A3.5,3.5,0,1,1,15.5,12,3.5,3.5,0,0,1,12,15.5Z"/>
+              <svg
+                className="swap-off h-7 w-7 xl:h-9 xl:w-9 fill-current text-slate-600"
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 24 24"
+              >
+                <path d="M5.64,17l-.71.71a1,1,0,0,0,0,1.41,1,1,0,0,0,1.41,0l.71-.71A1,1,0,0,0,5.64,17ZM5,12a1,1,0,0,0-1-1H3a1,1,0,0,0,0,2H4A1,1,0,0,0,5,12Zm7-7a1,1,0,0,0,1-1V3a1,1,0,0,0-2,0V4A1,1,0,0,0,12,5ZM5.64,7.05a1,1,0,0,0,.7.29,1,1,0,0,0,.71-.29,1,1,0,0,0,0-1.41l-.71-.71A1,1,0,0,0,4.93,6.34Zm12,.29a1,1,0,0,0,.7-.29l.71-.71a1,1,0,1,0-1.41-1.41L17,5.64a1,1,0,0,0,0,1.41A1,1,0,0,0,17.66,7.34ZM21,11H20a1,1,0,0,0,0,2h1a1,1,0,0,0,0-2Zm-9,8a1,1,0,0,0-1,1v1a1,1,0,0,0,2,0V20A1,1,0,0,0,12,19ZM18.36,17A1,1,0,0,0,17,18.36l.71.71a1,1,0,0,0,1.41,0,1,1,0,0,0,0-1.41ZM12,6.5A5.5,5.5,0,1,0,17.5,12,5.51,5.51,0,0,0,12,6.5Zm0,9A3.5,3.5,0,1,1,15.5,12,3.5,3.5,0,0,1,12,15.5Z" />
               </svg>
 
               {/* Moon Icon */}
-              <svg className="swap-on h-7 xl:h-9 w-7 xl:w-9 fill-current text-slate-600" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
-                <path d="M21.64,13a1,1,0,0,0-1.05-.14,8.05,8.05,0,0,1-3.37.73A8.15,8.15,0,0,1,9.08,5.49a8.59,8.59,0,0,1,.25-2A1,1,0,0,0,8,2.36,10.14,10.14,0,1,0,22,14.05,1,1,0,0,0,21.64,13Zm-9.5,6.69A8.14,8.14,0,0,1,7.08,5.22v.27A10.15,10.15,0,0,0,17.22,15.63a9.79,9.79,0,0,0,2.1-.22A8.11,8.11,0,0,1,12.14,19.73Z"/>
+              <svg
+                className="swap-on h-7 xl:h-9 w-7 xl:w-9 fill-current text-slate-600"
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 24 24"
+              >
+                <path d="M21.64,13a1,1,0,0,0-1.05-.14,8.05,8.05,0,0,1-3.37.73A8.15,8.15,0,0,1,9.08,5.49a8.59,8.59,0,0,1,.25-2A1,1,0,0,0,8,2.36,10.14,10.14,0,1,0,22,14.05,1,1,0,0,0,21.64,13Zm-9.5,6.69A8.14,8.14,0,0,1,7.08,5.22v.27A10.15,10.15,0,0,0,17.22,15.63a9.79,9.79,0,0,0,2.1-.22A8.11,8.11,0,0,1,12.14,19.73Z" />
               </svg>
             </label>
 
@@ -67,20 +89,77 @@ const Navbar = () => {
               href="/resume.pdf"
               download
               className="hidden lg:flex items-center gap-3 text-sm rounded-full border border-gray-500 ml-4 hover:bg-[#7081C8] hover:text-white px-8 py-2.5"
-              onMouseEnter={() => setIsHovered(true)} 
+              onMouseEnter={() => setIsHovered(true)}
               onMouseLeave={() => setIsHovered(false)}
             >
-              Resume <img src={isHovered ? arrowIconDark : arrowIcon} className="w-3" alt="Arrow icon" />
+              Resume{" "}
+              <img
+                src={isHovered ? arrowIconDark : arrowIcon}
+                className="w-3"
+                alt="Arrow icon"
+              />
             </a>
 
             {/* Mobile Dropdown */}
-            <div className="dropdown">
-              <div tabIndex={0} role="button" className="btn btn-ghost btn-circle lg:hidden">
-                <img src={menuBlack} alt="" className="w-5" />
-              </div>
-              <ul tabIndex={0} className="menu menu-sm dropdown-content gap-1 bg-base-100 rounded-box z-10 mt-3 w-52 p-2 text-center">
-                {navLinks}
-              </ul>
+            {/* Mobile Menu */}
+            <div className="lg:hidden">
+              <details className="relative">
+                <summary className="list-none cursor-pointer">
+                  <div className="p-2 rounded-lg hover:bg-gray-100 transition">
+                    <img src={menuBlack} alt="Menu" className="w-6" />
+                  </div>
+                </summary>
+
+                <div className="absolute right-0 mt-4 w-[85vw] max-w-sm bg-white rounded-2xl shadow-xl border border-gray-100 p-6">
+                  <ul className="flex flex-col gap-5 text-lg font-medium text-gray-700">
+                    <li>
+                      <HashLink
+                        smooth
+                        to="/#home"
+                        className="hover:text-[#7081C8] transition"
+                      >
+                        Home
+                      </HashLink>
+                    </li>
+                    <li>
+                      <HashLink
+                        smooth
+                        to="/#about"
+                        className="hover:text-[#7081C8] transition"
+                      >
+                        About Me
+                      </HashLink>
+                    </li>
+                    <li>
+                      <HashLink
+                        smooth
+                        to="/#projects"
+                        className="hover:text-[#7081C8] transition"
+                      >
+                        Projects
+                      </HashLink>
+                    </li>
+                    <li>
+                      <HashLink
+                        smooth
+                        to="/#education"
+                        className="hover:text-[#7081C8] transition"
+                      >
+                        Education
+                      </HashLink>
+                    </li>
+                    <li>
+                      <HashLink
+                        smooth
+                        to="/#contact"
+                        className="hover:text-[#7081C8] transition"
+                      >
+                        Contact Me
+                      </HashLink>
+                    </li>
+                  </ul>
+                </div>
+              </details>
             </div>
           </div>
         </div>
